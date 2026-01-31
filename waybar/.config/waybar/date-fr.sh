d539570dc664ecef7764599c6548026be1fdaf41
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2026-01-31 15:26:46 -0500

    Various improvements in Niri and Waybar config.

--- waybar/.config/waybar/date-fr.sh
+++ waybar/.config/waybar/date-fr.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-LC_ALL=fr_CA.utf8 date '+%A %Y-%m-%d %H:%M'
+# Capitalize first letter of the day
+day=$(LC_ALL=fr_CA.utf8 date '+%A %Y-%m-%d %H:%M')
+echo "${day^}"
--- waybar/.config/waybar/start-swayidle.sh
+++ waybar/.config/waybar/start-swayidle.sh
@@ -8,9 +8,9 @@ pkill -x swayidle 2>/dev/null
 
 # Start swayidle with your configuration
 exec swayidle -w \
-    timeout 285 "notify-send -u critical -t 15000 -i ~/.config/waybar/caution_4539472.png '🔒 Verrouillage imminent' 'L՚écran se verrouillera dans 15 secondes'" \
+    timeout 570 "notify-send -u critical -t 30000 -i ~/.config/waybar/caution_4539472.png '🔒 Verrouillage imminent' 'L՚écran se verrouillera dans 30 secondes'" \
     resume 'swaync-client -C' \
-    timeout 300 'swaylock -f' \
-    timeout 600 'systemctl suspend' \
+    timeout 600 'swaylock -f' \
+    timeout 900 'systemctl suspend' \
     before-sleep 'swaylock -f' \
     after-resume "$HOME/nixos-config/bin/resume-actions.sh"
